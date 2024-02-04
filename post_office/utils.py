bff53ab2dbdae5f9420d28e41402c743917a0d88
Ronan Boiteau <ronan@boiteau.eu>
2024-02-04 17:17:17 +0100

    Coding style

--- post_office/utils.py
+++ post_office/utils.py
@@ -157,7 +157,7 @@ def cleanup_expired_mails(cutoff_date, delete_attachments=True, batch_size=1000)
     expired_emails_ids = Email.objects.filter(created__lt=cutoff_date).values_list('id', flat=True)
     email_id_batches = split_emails(expired_emails_ids, batch_size)
     total_deleted_emails = 0
-
+    
     for email_ids in email_id_batches:
         # Delete email and incr total_deleted_emails counter
         _, deleted_data = Email.objects.filter(id__in=email_ids).delete()
